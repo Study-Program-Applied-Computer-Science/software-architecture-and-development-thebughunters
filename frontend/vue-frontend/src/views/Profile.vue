699ea7ed4ea345f16ac1f8cb5265315b530324eb
Yathish <yathish2012@gmail.com>
2025-01-18 13:34:08 +0100

    feat Frontend vue structure with components

---
+++ frontend/vue-frontend/src/views/Profile.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="container-fluid">
+    <div
+      class="mt-4 page-header min-height-300 border-radius-xl"
+      :style="{
+        backgroundImage:
+          'url(' + require('@/assets/img/curved-images/pleasantgrocery.jpg') + ')',
+        backgroundPositionY: '50%',
+      }"
+    >
+      <span class="mask bg-gradient-dark opacity-6"></span>
+    </div>
+    <div class="mx-4 overflow-hidden card card-body blur shadow-blur mt-n6">
+      <div class="row gx-4">
+        <div class="col-auto">
+          <div class="avatar avatar-xl position-relative">
+            <img
+              src="@/assets/img/bruce-mars.jpg"
+              alt="profile_image"
+              class="shadow-sm w-100 border-radius-lg"
+            />
+          </div>
+        </div>
+        <div class="col-auto my-auto">
+          <div class="h-100">
+            <h5 class="mb-1">Peter </h5>
+            <p class="mb-0 text-sm font-weight-bold">End User</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-4 container-fluid">
+    <div class="mt-3 row">
+      <div class="mt-4 col-12 col-md-6 col-xl-4 mt-md-0">
+        <profile-info-card
+          title="Profile Information"
+          description="Hi, I’m Peter, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
+          :info="{
+            fullName: 'Peter Dillinger',
+            mobile: '(44) 123 1234 123',
+            email: 'Peter-Dillinger@mail.com',
+            location: 'Germany',
+          }"
+          :social="[
+            {
+              link: 'https://www.facebook.com/CreativeTim/',
+              icon: faFacebook,
+            },
+            {
+              link: 'https://twitter.com/creativetim',
+              icon: faTwitter,
+            },
+            {
+              link: 'https://www.instagram.com/creativetimofficial/',
+              icon: faInstagram,
+            },
+          ]"
+          :action="{
+            route: 'javascript:;',
+            tooltip: 'Edit Profile',
+          }"
+        />
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import ProfileInfoCard from "./components/ProfileInfoCard.vue";
+import sophie from "@/assets/img/kal-visuals-square.jpg";
+import marie from "@/assets/img/marie.jpg";
+import ivana from "@/assets/img/ivana-square.jpg";
+import peterson from "@/assets/img/team-4.jpg";
+import nick from "@/assets/img/team-3.jpg";
+import img1 from "@/assets/img/home-decor-1.jpg";
+import img2 from "@/assets/img/home-decor-2.jpg";
+import img3 from "@/assets/img/home-decor-3.jpg";
+import team1 from "@/assets/img/team-1.jpg";
+import team2 from "@/assets/img/team-2.jpg";
+import team3 from "@/assets/img/team-3.jpg";
+import team4 from "@/assets/img/team-4.jpg";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import setNavPills from "@/assets/js/nav-pills.js";
+import setTooltip from "@/assets/js/tooltip.js";
+
+export default {
+  name: "ProfileOverview",
+  components: {
+    ProfileInfoCard,
+  },
+  data() {
+    return {
+      showMenu: false,
+      sophie,
+      marie,
+      ivana,
+      peterson,
+      nick,
+      img1,
+      team1,
+      team2,
+      team3,
+      team4,
+      img2,
+      img3,
+      faFacebook,
+      faTwitter,
+      faInstagram,
+    };
+  },
+
+  mounted() {
+    this.$store.state.isAbsolute = true;
+    setNavPills();
+    setTooltip(this.$store.state.bootstrap);
+  },
+  beforeUnmount() {
+    this.$store.state.isAbsolute = false;
+  },
+};
+</script>
