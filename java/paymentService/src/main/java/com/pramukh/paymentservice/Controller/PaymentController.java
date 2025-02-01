26d885cbbf575ef9203ef9e3e4656b3ddfa9c558
Pramukh Prakash <pramukhp35@gmail.com>
2025-02-01 10:57:21 +0100

    feat(payment) adding rate limiters

--- java/paymentService/src/main/java/com/pramukh/paymentservice/Controller/PaymentController.java
+++ java/paymentService/src/main/java/com/pramukh/paymentservice/Controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.pramukh.paymentservice.DTO.PaymentRequestDto;
 import com.pramukh.paymentservice.DTO.PaymentResponseDto;
 import com.pramukh.paymentservice.Service.StripeService;
 import com.stripe.exception.StripeException;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,10 @@ public class PaymentController {
     private StripeService stripeService;
 
     @PostMapping("/payment")
+    @RateLimiter(name = "paymentRateLimiter")
     @Operation(summary = "Make payment")
     public ResponseEntity<PaymentResponseDto> makePayment(@RequestBody PaymentRequestDto paymentRequestDto) throws StripeException {
+        System.out.println("Payment triggeredd");
         PaymentResponseDto paymentResponseDto = stripeService.makepayment(paymentRequestDto);
         return ResponseEntity.ok(paymentResponseDto);
     }
